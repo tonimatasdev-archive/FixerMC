c0776e3f2466b824cc5a179d4f7d344ebb058424
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-11-21 23:02:08 +0100

    Better?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    application
+    //id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "dev.tonimatas"
@@ -9,9 +11,24 @@ repositories {
     mavenCentral()
 }
 
+sourceSets.main.get().resources.srcDirs("src/main/resources")
+
 dependencies {
     implementation("fr.flowarg:flowupdater:1.9.2")
     implementation("fr.flowarg:openlauncherlib:3.2.11")
     implementation("net.raphimc:MinecraftAuth:4.1.1")
     implementation("com.formdev:flatlaf:3.5.2")
+    //implementation("org.openjfx:javafx-controls:23")
+    //implementation("org.openjfx:javafx-fxml:23")
+    //implementation("org.openjfx:javafx-web:23")
 }
+
+application {
+    mainClass = "dev.tonimatas.fixermc.Main"
+}
+
+
+//javafx {
+//    version = "23"
+//    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web")
+//}
